b1edea1952cb658cc80ddb5a89afbfb91a381cb7
Saharsh Cherian <saharshdc@gmail.com>
2023-03-16 11:50:37 -0400

    added conditional rendering of navtabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/navtabs.css";
 
 const NavTabs = () => {
+	const location = useLocation();
+  const page = location.pathname
+console.log(location)
+
   return (
     <div className="navtabs"> 
     {/* render only if page location is notthe same as link */}
+        
      <Link to="/"> <h2 id="nav-btn">Back Home</h2>
       </Link>
-      <Link to="/List"> <h2 id="nav-btn">A-Z</h2>
-      </Link>
-      <Link to="/Bar"> <h2 id="nav-btn">What's in your bar?</h2>
-      </Link>
+     
+      {page=== "/List" ? null : <Link to="/List"> <h2 id="nav-btn">A-Z</h2>
+      </Link> }
+      {page=== "/Bar" ? null : <Link to="/Bar"> <h2 id="nav-btn">What's in your bar?</h2>
+      </Link>}
 </div>  )
 }
 
