c1a2e471b04a1f0709baac9888d30eb9e61272a4
Saharsh Cherian <saharshdc@gmail.com>
2023-03-17 11:41:27 -0400

    added sound effect to main menu and fixed nav tab sacing on mobile

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,20 +3,27 @@ import { Link } from "react-router-dom";
 import "../styles/landing.css";
 import "animate.css";
 // import shaker component
+import clinkSound from "../assets/clinksound.mp3";
 
 const Landing = () => {
+  const playSound = () => {
+    const audio = new Audio(clinkSound);
+    audio.play();
+  };
+
   return (
     <div className="main-splash animate__animated animate__fadeIn">
  
+     
       <div className="main-nav">
-        <Link to="/random">
+        <Link to="/Random" onClick={playSound} >
           
           <h2 className="main-tile tile-random">Random</h2>
                   </Link>
-        <Link to="/List">
+        <Link to="/List" onClick={playSound}>
           <h2 className="main-tile tile-list">A-Z</h2>
         </Link>
-        <Link to="/Bar">
+        <Link to="/Bar" onClick={playSound}>
         
           <h2 className="main-tile tile-bar">Bar</h2>
         </Link>
