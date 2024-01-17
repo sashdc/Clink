6b75ba9dfe1d539fdc462504b10450f7022e2134
sashdc <saharshdc@gmail.com>
2024-01-16 20:49:59 -0500

    changed landing page styling for readability

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -17,25 +17,33 @@ const Landing = () => {
         <div className="main-nav animate__animated animate__fadeIn">
         <Link to="/Random" onClick={playSound}>
           <div className="main-tile tile-random">
+            <div className="title-bar">
           <h2 className="tile-title">Random</h2>
+          </div>
             <img className="tile-img" src="../images/cocktail.webp" alt="random" />
           </div>
         </Link>
         <Link to="/List" onClick={playSound}>
         <div className="main-tile tile-list">
+        <div className="title-bar">
           <h2 className="tile-title">A-Z</h2>
+          </div>
             <img className="tile-img" src="../images/cocktails.webp" alt="list" />
           </div>
         </Link>
         <Link to="/Bar" onClick={playSound}>
         <div className="main-tile tile-bar">
+        <div className="title-bar">
           <h2 className="tile-title">Bar</h2>
+          </div>
             <img className="tile-img" src="../images/bottles.webp" alt="bar" />
           </div>
         </Link>
         <Link to="/Favourites" onClick={playSound}>
         <div className="main-tile tile-fav">
+        <div className="title-bar">
           <h2 className="tile-title">Favourites</h2>
+          </div>
             <img className="tile-img" src="../images/fav.webp" alt="favourites" />
           </div>
         </Link>
